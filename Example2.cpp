62ab4b31c11e5cefd1753713ae62be09e44bd803
Abdul Mateen <98536971+I-am-Mateen@users.noreply.github.com>
2022-02-17 08:19:43 +0500

    Add files via upload

---
+++ Example2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    char friends;
+    cout << "Are your friends going to university: (Press Y for yes) ";
+    cin >> friends;
+    if (friends == 'y' || friends == 'Y')
+    {
+        cout << "Go to university";
+    }
+}
